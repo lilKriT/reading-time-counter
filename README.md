c616ac0818428227833acb38d1f3f293515ed0db
Piotr Filipiak <kiddhkane@gmail.com>
2022-11-21 18:20:38 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# WP Reading Time Counter
+
+A second in the series of plugins from Udemy course.
+Created by lilKriT.
---
+++ counter.php
@@ -0,0 +1,9 @@
+<?php
+
+/*
+    Plugin Name: Reading Time Counter
+    Description: A plugin that counts letters, words and approximate reading time for an article.
+    Version: 1.0
+    Author: lilKriT
+    Author URI: https://lilkrit.dev
+*/
