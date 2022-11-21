2b3d5d896497547163d1d2a46a88a271bf588457
Piotr Filipiak <kiddhkane@gmail.com>
2022-11-21 19:54:30 +0100

    Moved the plugin into it's own class.

--- counter.php
+++ counter.php
@@ -8,15 +8,30 @@
     Author URI: https://lilkrit.dev
 */
 
-function counterSettingsLink()
+// 
+class ReadingTimeCounter
 {
-    add_options_page("Reading Time Counter", "RT Counter", "manage_options", "rt-counter-settings", "settingsPageHTML");    // title, name in link list, permissions, slug, function
+    function __construct()
+    {
+        add_action("admin_menu", array($this, "adminPage"));
+    }
+
+    function adminPage()
+    {
+        add_options_page("Reading Time Counter", "RT Counter", "manage_options", "rt-counter-settings", array($this, "settingsPageHTML"));    // title, name in link list, permissions, slug, function
+    }
+
+    function settingsPageHTML()
+    { ?>
+
+        <div class="wrap">
+            <h1>Reading Time Counter Settings</h1>
+            <p>Settings here.</p>
+        </div>
+
+<?php
+    }
 }
-add_action("admin_menu", "counterSettingsLink");
 
-function settingsPageHTML()
-{ ?>
-
-    Hello World, plugin
-
-<?php }
+// Actually created an instance
+$readingTimeCounter = new ReadingTimeCounter();
